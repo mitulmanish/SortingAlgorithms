b5600fa7859ae505562474a9cf4a76ca8cd43727
Mitul Manish <mitul.manish@gmail.com>
2016-07-24 13:04:38 +1000

    Initial Commit

---
+++ SortingAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SortingAlgorithms
+//
+//  Created by Mitul Manish on 24/07/2016.
+//  Copyright © 2016 Mitul Manish. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
